64905f2ff238db4a68677c62182015e9e290f886
Ganganath Gunawardana <46973603+Ganganath92@users.noreply.github.com>
2020-08-15 13:28:57 +0530

    exam
    item 4 done

--- app/src/HostingType.php
+++ app/src/HostingType.php
@@ -2,6 +2,7 @@
 
 use SilverStripe\ORM\DataObject;
 use HostingContract;
+use SilverStripe\Forms\RequiredFields;
 
 class HostingType extends DataObject {
 
@@ -15,3 +16,39 @@ class HostingType extends DataObject {
 		'HostingContracts' => HostingContract::class,
 	);
 }
+
+//Ganganath Gunawardane - item 4
+class MinPriceFields extends RequiredFields {
+
+    protected $minPrice = [];
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        $required = func_get_args();
+        if(isset($required[0]) && is_array($required[0])) {
+            $required = $required[0];
+        }
+        $this->minPrice = $required;
+        parent::__construct($required);
+    }
+
+    function php($data) {
+        $valid = parent::php($data);
+
+        if (!is_numeric($data[$this->minPrice[0]])) {
+            $this->validationError(
+                $this->minPrice[0], 'Not a number. Your number must be greater than or equal to 5', 'validation', false
+            );
+            $valid = false;
+        } elseif ($data[$this->minPrice[0]] < 5) {
+            $this->validationError(
+                $this->minPrice[0], 'Your number must be greater than or equal to 5', 'validation', false
+            );
+            $valid = false;
+        }
+        return $valid;
+    }
+}
+//Ganganath Gunawardane - item 4
